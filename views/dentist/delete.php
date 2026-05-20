4d4cb6318f1e65b58f51bdbad5c983bcc4c81363
wwwrwrwr <kakgaa10@mail.ru>
2026-05-20 13:49:59 +0300

    Create delete.php

---
+++ views/dentist/delete.php
@@ -0,0 +1,13 @@
+<div class="container mt-4">
+  <h2>Удаление врача</h2>
+  <div class="alert alert-danger">
+    Вы уверены что хотите удалить врача
+    <strong><?= htmlspecialchars($dentist['last_name'] . ' ' . $dentist['first_name']) ?></strong>?
+  </div>
+  <form method="post">
+    <input type="hidden" name="csrf_token"
+           value="<?= htmlspecialchars(generateCsrfToken()) ?>">
+    <button type="submit" class="btn btn-danger">Удалить</button>
+    <a href="?entity=dentist&action=list" class="btn btn-secondary">Отмена</a>
+  </form>
+</div>
