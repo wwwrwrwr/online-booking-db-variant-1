bcd8f801634a009cbb10a5e7d1592a18907cc603
wwwrwrwr <kakgaa10@mail.ru>
2026-05-20 13:30:32 +0700

    Delete test_db.php

--- test_db.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'src/Database.php';
-try {
-    $pdo = Database::getConnection();
-    echo "✅ Подключение успешно! Версия MySQL: " . $pdo->getAttribute(PDO::ATTR_SERVER_VERSION);
-} catch (PDOException $e) {
-    echo "❌ Ошибка: " . $e->getMessage();
-}
