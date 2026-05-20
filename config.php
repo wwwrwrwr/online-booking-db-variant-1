7627e905564ea346f65cb138cca2aa70dc911de6
wwwrwrwr <kakgaa10@mail.ru>
2026-05-20 13:58:53 +0300

    Create config.php

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+// config.php — настройки подключения к БД Beget
+return [
+    'host'     => 'localhost',
+    'dbname'   => 'w92350sl_1',
+    'username' => 'w92350sl_1',
+    'password' => '',
+    'charset'  => 'utf8mb4'
+];
