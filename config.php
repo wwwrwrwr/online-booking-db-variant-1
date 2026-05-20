42641e9aec598235efde8637f4e2487853e31d18
wwwrwrwr <kakgaa10@mail.ru>
2026-05-20 14:03:44 +0300

    Update config.php

--- config.php
+++ config.php
@@ -4,6 +4,6 @@ return [
     'host'     => 'localhost',
     'dbname'   => 'w92350sl_1',
     'username' => 'w92350sl_1',
-    'password' => '',
+    'password' => 'StrongPass910',
     'charset'  => 'utf8mb4'
 ];
